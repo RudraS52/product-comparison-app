d3b389452a5a68e4b1b254f030add51a529c98e8
Rudra Singh <rpsingh.developer@gmail.com>
2025-06-13 11:19:24 +0530

    Fixed product card to show better in mobile view and brand and price labels

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,29 +1,32 @@
+
 import React from 'react';
 
 const ProductCard = ({ product, onCompareToggle, isSelected }) => {
   return (
     <div className="product-card">
       <div className="zoom-container">
-  <img src={product.image} alt={product.name} />
-</div>
+        <img src={product.image} alt={product.name} />
+      </div>
 
-<h3>{product.name}</h3>
-      <p>{product.brand}</p>
-      <p>{product.price}</p>
+      <h3>{product.name}</h3>
+      <p><strong>Brand:</strong> {product.brand}</p>
+      <p><strong>Price:</strong> {product.price}</p>
+
       <ul>
         {product.features.map((feature, i) => (
           <li key={i}>{feature}</li>
         ))}
       </ul>
-    <button
-  className={isSelected ? "selected" : ""}
-  onClick={() => onCompareToggle(product.id)}
->
-  {isSelected ? "Added for Compare" : "Add to Compare"}
-</button>
 
- </div>
+      <button
+        className={isSelected ? "selected" : ""}
+        onClick={() => onCompareToggle(product.id)}
+      >
+        {isSelected ? "Added for Compare" : "Add to Compare"}
+      </button>
+    </div>
   );
 };
 
 export default ProductCard;
+
